c4515c0baffb999f240a296481595532a541e264
yangle <yangle@luojilab.com>
2022-06-12 17:12:24 +0800

    5.2.4 范例: 跃动的线

--- main.cpp
+++ main.cpp
@@ -1,100 +1,57 @@
 #define GL_SILENCE_DEPRECATION
 
-#include <osg/Texture2D>
+#include <osg/LineWidth>
 #include <osg/Geometry>
 #include <osg/Geode>
-#include <osgDB/ReadFile>
-#include <osgUtil/SmoothingVisitor>
 #include <osgViewer/Viewer>
 
-osg::Drawable *createHouseWall()
+class DynamicLineCallback : public osg::Drawable::UpdateCallback
 {
-  // house vertices
-  osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
-  vertices->push_back(osg::Vec3(0.0, 0.0, 4.0));
-  vertices->push_back(osg::Vec3(0.0, 0.0, 0.0));
-  vertices->push_back(osg::Vec3(6.0, 0.0, 4.0));
-  vertices->push_back(osg::Vec3(6.0, 0.0, 0.0));
-  vertices->push_back(osg::Vec3(6.0, 4.0, 4.0));
-  vertices->push_back(osg::Vec3(6.0, 4.0, 0.0));
-  vertices->push_back(osg::Vec3(0.0, 4.0, 4.0));
-  vertices->push_back(osg::Vec3(0.0, 4.0, 0.0));
-  vertices->push_back(osg::Vec3(0.0, 0.0, 4.0));
-  vertices->push_back(osg::Vec3(0.0, 0.0, 0.0));
-  // house normal
-  osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array(10);
-  (*normals)[0].set(-0.707, -0.707, 0.0);
-  (*normals)[1].set(-0.707, -0.707, 0.0);
-  (*normals)[2].set( 0.707, -0.707, 0.0);
-  (*normals)[3].set( 0.707, -0.707, 0.0);
-  (*normals)[4].set( 0.707,  0.707, 0.0);
-  (*normals)[5].set( 0.707,  0.707, 0.0);
-  (*normals)[6].set(-0.707,  0.707, 0.0);
-  (*normals)[7].set(-0.707,  0.707, 0.0);
-  (*normals)[8].set(-0.707, -0.707, 0.0);
-  (*normals)[9].set(-0.707, -0.707, 0.0);
-  // house texture coordinates
-  osg::ref_ptr<osg::Vec2Array> texcoords = new osg::Vec2Array(10);
-  (*texcoords)[0].set(0.0, 1.0);
-  (*texcoords)[1].set(0.0, 0.0);
-  (*texcoords)[2].set(0.3, 1.0);
-  (*texcoords)[3].set(0.3, 0.0);
-  (*texcoords)[4].set(0.5, 1.0);
-  (*texcoords)[5].set(0.5, 0.0);
-  (*texcoords)[6].set(0.8, 1.0);
-  (*texcoords)[7].set(0.8, 0.0);
-  (*texcoords)[8].set(1.0, 1.0);
-  (*texcoords)[9].set(1.0, 0.0);
-  // create wall geometry
-  osg::ref_ptr<osg::Geometry> houseWall = new osg::Geometry;
-  houseWall->setVertexArray(vertices.get());
-  houseWall->setTexCoordArray(0, texcoords.get());
-  houseWall->setNormalArray(normals.get());
-  houseWall->setNormalBinding(osg::Geometry::BIND_PER_VERTEX);
-  houseWall->addPrimitiveSet(new osg::DrawArrays(osg::DrawArrays::QUAD_STRIP, 0, 10));
-  houseWall->getOrCreateStateSet()->setTextureAttributeAndModes(0, new osg::Texture2D(osgDB::readImageFile("wall.png")));
-  return houseWall.release();
-}
+public:
+  DynamicLineCallback() : _angle(0.0) {}
 
-osg::Drawable *createHouseRoof()
-{
-  // house vertices
-  osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array;
-  vertices->push_back(osg::Vec3(-0.2, -0.5, 3.5));
-  vertices->push_back(osg::Vec3( 6.2, -0.5, 3.5));
-  vertices->push_back(osg::Vec3( 0.8,  2.0, 6.0));
-  vertices->push_back(osg::Vec3( 5.2,  2.0, 6.0));
-  vertices->push_back(osg::Vec3(-0.2,  4.5, 3.5));
-  vertices->push_back(osg::Vec3( 6.2,  4.5, 3.5));
-  // roof primitives
-  osg::ref_ptr<osg::DrawArrays> roof = new osg::DrawArrays(osg::DrawArrays::QUAD_STRIP, 0, 6);
-  osg::ref_ptr<osg::DrawElementsUInt> roofSide = new osg::DrawElementsUInt(osg::DrawElementsUInt::TRIANGLES, 6);
-  (*roofSide)[0] = 0;
-  (*roofSide)[1] = 2;
-  (*roofSide)[2] = 4;
-  (*roofSide)[3] = 5;
-  (*roofSide)[4] = 3;
-  (*roofSide)[5] = 1;
-  // color
-  osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
-  colors->push_back(osg::Vec4(0.25, 0.0, 0.0, 1.0));
-  // create roof geometry
-  osg::ref_ptr<osg::Geometry> houseRoof = new osg::Geometry;
-  houseRoof->setVertexArray(vertices.get());
-  houseRoof->setColorArray(colors.get());
-  houseRoof->setColorBinding(osg::Geometry::BIND_OVERALL);
-  houseRoof->addPrimitiveSet(roof.get());
-  houseRoof->addPrimitiveSet(roofSide.get());
-  osgUtil::SmoothingVisitor smv;
-  smv.smooth(*houseRoof);
-  return houseRoof.release();
-}
+  virtual void update(osg::NodeVisitor *nv, osg::Drawable *drawable)
+  {
+    osg::Geometry *geom = dynamic_cast<osg::Geometry *>(drawable);
+    if (!geom)
+      return;
 
-int main(int argc, char** argv)
+    osg::Vec3Array *vertices = dynamic_cast<osg::Vec3Array *>(geom->getVertexArray());
+    if (vertices)
+    {
+      for (osg::Vec3Array::iterator itr = vertices->begin(); itr != vertices->end() - 1; ++itr)
+        itr->set((*(itr + 1)));
+
+      _angle += 1.0 / 10.0;
+      osg::Vec3 &pt = vertices->back();
+      pt.set(10.0 * cos(_angle), 0.0, 10.0 * sin(_angle));
+      vertices->dirty();
+    }
+  }
+
+protected:
+  float _angle;
+};
+
+int main(int argc, char **argv)
 {
+  osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(10);
+  for (unsigned int i = 0; i < 10; ++i)
+    (*vertices)[i].set(float(i), 0.0, 0.0);
+
+  osg::ref_ptr<osg::Geometry> lineGeom = new osg::Geometry;
+  lineGeom->setVertexArray(vertices.get());
+  lineGeom->addPrimitiveSet(new osg::DrawArrays(osg::DrawArrays::LINE_STRIP, 0, 10));
+
+  lineGeom->setInitialBound(osg::BoundingBox(osg::Vec3(-10.0, -10.0, -10.0), osg::Vec3(10.0, 10.0, 10.0)));
+  lineGeom->setUpdateCallback(new DynamicLineCallback);
+  lineGeom->setUseDisplayList(false);
+  lineGeom->setUseVertexBufferObjects(true);
+
   osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-  geode->addDrawable(createHouseWall());
-  geode->addDrawable(createHouseRoof());
+  geode->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+  geode->getOrCreateStateSet()->setAttribute(new osg::LineWidth(2.0));
+  geode->addDrawable(lineGeom.get());
 
   osgViewer::Viewer viewer;
   viewer.setSceneData(geode.get());
