1803d5bddceb4613af8e2ff3522854034518b338
yangle <yangle@luojilab.com>
2022-06-12 11:40:22 +0800

    4.3.7 范例: 节点代理

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #define GL_SILENCE_DEPRECATION
 
-#include <osg/LOD>
+#include <osg/ProxyNode>
 #include <osgDB/ReadFile>
 #include <osgViewer/Viewer>
 
 int main(int argc, char** argv)
 {
-  // exmpale data里没有bunny文件, 可以用其它文件代替
-  // 另外, 我不知道怎么移动摄像机来改变距离
-  osg::Node *model = osgDB::readNodeFile("bunny-high.ive");
-  float r = model->getBound().radius();
+  osg::ArgumentParser arguments(&argc, argv);
+  osg::ref_ptr<osg::ProxyNode> root = new osg::ProxyNode;
 
-  osg::ref_ptr<osg::LOD> root = new osg::LOD;
-  root->addChild(osgDB::readNodeFile("bunny-low.ive"), r*7, FLT_MAX);
-  root->addChild(osgDB::readNodeFile("bunny-mid.ive"), r*3, r*7);
-  root->addChild(model, 0.0, r*3);
-
+  unsigned int childrenNo = 0;
+  for (int i = 1; i < arguments.argc(); ++i)
+  {
+    if (arguments.isString(i))
+      root->setFileName(childrenNo++, arguments[i]);
+  }
+  if (!root->getNumFileNames())
+    root->setFileName(0, "cow.osgt");
+  
   osgViewer::Viewer viewer;
   viewer.setSceneData(root.get());
   return viewer.run();
