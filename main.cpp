dc6988402791a6e6e5d62002021789abdb6d4fe7
yangle <yangle@luojilab.com>
2022-06-12 20:11:13 +0800

    5.4.4 范例: 一首古诗

--- main.cpp
+++ main.cpp
@@ -1,29 +1,60 @@
 #define GL_SILENCE_DEPRECATION
 
-#include <osg/DrawPixels>
 #include <osg/Geode>
-#include <osgDB/ReadFile>
+#include <osg/Geometry>
+#include <osgText/Text>
 #include <osgViewer/Viewer>
+#include <locale.h>
+#include <string>
 
-int main(int argc, char** argv) {
-  osg::ref_ptr<osg::DrawPixels> bitmap1 = new osg::DrawPixels;
-  bitmap1->setPosition(osg::Vec3(0.0, 0.0, 0.0));
-  bitmap1->setImage(osgDB::readImageFile("Images/osg64.png"));
+using std::string;
 
-  osg::ref_ptr<osg::DrawPixels> bitmap2 = new osg::DrawPixels;
-  bitmap2->setPosition(osg::Vec3(80.0, 0.0, 0.0));
-  bitmap2->setImage(osgDB::readImageFile("Images/osg128.png"));
+void setupProperties(osgText::Text &textObject, osgText::Font *font, float size, const osg::Vec3 &pos)
+{
+  textObject.setFont(font);
+  textObject.setCharacterSize(size);
+  textObject.setPosition(pos);
+  textObject.setColor(osg::Vec4(0.0, 0.0, 1.0, 1.0));
+  textObject.setAlignment(osgText::Text::CENTER_BOTTOM);
+  textObject.setAxisAlignment(osgText::Text::XZ_PLANE);
+}
 
-  osg::ref_ptr<osg::DrawPixels> bitmap3 = new osg::DrawPixels;
-  bitmap3->setPosition(osg::Vec3(200.0, 0.0, 0.0));
-  bitmap3->setImage(osgDB::readImageFile("Images/osg256.png"));
-  bitmap3->setSubImageDimensions(64, 64, 128, 128);
-  bitmap3->setUseSubImage(true);
+void createContent(osgText::Text &textObject, const string string)
+{
+  int requiredSize = std::mbstowcs(NULL, string.c_str(), 0);
+  wchar_t *wtext = new wchar_t[requiredSize + 1];
+  std::mbstowcs(wtext, string.c_str(), requiredSize + 1);
+  textObject.setText(wtext);
+  delete[] wtext;
+}
+
+int main(int argc, char **argv)
+{
+  setlocale(LC_ALL, "zh_CN");
+  const char *titleString = "木兰辞\n拟古决绝词柬友";
+  const char *textString = {
+      "人生若只如初见, 何事秋风悲画扇; \n"
+      "等闲变却故人心, 却道故人心易变; \n"
+      "骊山语罢清宵半, 夜雨霖铃终不怨; \n"
+      "何如薄幸锦衣郎, 比翼连枝当日愿; \n"};
+
+  osgText::Font *fontHei = osgText::readFontFile("fonts/arial.ttf");
+  osgText::Font *fontKai = osgText::readFontFile("fonts/times.ttf");
+
+  osg::ref_ptr<osgText::Text> title = new osgText::Text;
+  setupProperties(*title, fontHei, 20.0f, osg::Vec3(0.0f, 0.0f, 0.0f));
+  createContent(*title, titleString);
+
+  osg::ref_ptr<osgText::Text> text = new osgText::Text;
+  setupProperties(*text, fontKai, 15.0f, osg::Vec3(0.0f, 0.0f, -80.0f));
+  createContent(*text, textString);
 
   osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-  geode->addDrawable(bitmap1.get());
-  geode->addDrawable(bitmap2.get());
-  geode->addDrawable(bitmap3.get());
+  geode->addDrawable(osg::createTexturedQuadGeometry(
+      osg::Vec3(-150.0, 1.0, -130.0), osg::Vec3(300.0, 0.0, 0.0),
+      osg::Vec3(0.0, 0.0, 200.0), 1.0, 1.0));
+  geode->addDrawable(title.get());
+  geode->addDrawable(text.get());
 
   osgViewer::Viewer viewer;
   viewer.setSceneData(geode.get());
