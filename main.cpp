36533a6d09c2f3cad6fa813fabd1016edcb01f2d
yangle <yangle@luojilab.com>
2022-06-13 10:57:50 +0800

    8.1.4 范例: 处理键盘事件

--- main.cpp
+++ main.cpp
@@ -1,27 +1,62 @@
 #define GL_SILENCE_DEPRECATION
 
-#include <osg/Group>
+#include <osg/Switch>
 #include <osgDB/ReadFile>
-#include <osgViewer/CompositeViewer>
+#include <osgGA/GUIEventHandler>
 #include <osgViewer/Viewer>
+
+class KeyboardHandler : public osgGA::GUIEventHandler
+{
+public:
+  virtual bool handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &aa, osg::Object *, osg::NodeVisitor *)
+  {
+    osgViewer::Viewer *viewer = dynamic_cast<osgViewer::Viewer *>(&aa);
+    if (!viewer)
+      return false;
+
+    switch (ea.getEventType())
+    {
+    case osgGA::GUIEventAdapter::KEYDOWN:
+      if (ea.getKey() == osgGA::GUIEventAdapter::KEY_Space)
+      {
+        int width = ea.getWindowWidth();
+        int height = ea.getWindowHeight();
+        viewer->requestWarpPointer(width * 0.5, height * 0.5);
+      }
+      else
+      {
+        osg::Switch *root = dynamic_cast<osg::Switch *>(viewer->getSceneData());
+        if (!root)
+          return false;
+
+        if (ea.getKey() == '1')
+        {
+          root->setValue(0, true);
+          root->setValue(1, false);
+        }
+        else if (ea.getKey() == '2')
+        {
+          root->setValue(0, false);
+          root->setValue(1, true);
+        }
+        return true;
+      }
+      break;
+    default:
+      break;
+    }
+    return false;
+  }
+};
 
 int main(int argc, char **argv)
 {
-  osg::ref_ptr<osgViewer::Viewer> view1 = new osgViewer::Viewer;
-  view1->setUpViewInWindow(0, 50, 320, 240);
-  view1->setSceneData(osgDB::readNodeFile("cow.osg"));
+  osg::ref_ptr<osg::Switch> root = new osg::Switch;
+  root->addChild(osgDB::readNodeFile("cessna.osg"), true);
+  root->addChild(osgDB::readNodeFile("cessnafire.osg"), false);
 
-  osg::ref_ptr<osgViewer::Viewer> view2 = new osgViewer::Viewer;
-  view2->setUpViewInWindow(320, 50, 320, 240);
-  view2->setSceneData(osgDB::readNodeFile("cessna.osg"));
-
-  osg::ref_ptr<osgViewer::Viewer> view3 = new osgViewer::Viewer;
-  view3->setUpViewInWindow(640, 50, 320, 240);
-  view3->setSceneData(osgDB::readNodeFile("axes.osgt"));
-
-  osgViewer::CompositeViewer compositeViewer;
-  compositeViewer.addView(view1.get());
-  compositeViewer.addView(view2.get());
-  compositeViewer.addView(view3.get());
-  return compositeViewer.run();
+  osgViewer::Viewer viewer;
+  viewer.setSceneData(root.get());
+  viewer.addEventHandler(new KeyboardHandler);
+  return viewer.run();
 }
